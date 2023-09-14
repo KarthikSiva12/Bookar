a8085417e8264448a19e89fa7d9b189d2b40f1ce
KARTHIK SIVA S <122376253+KarthikSiva12@users.noreply.github.com>
2023-09-14 19:14:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const slides = document.querySelectorAll('.pstr');
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.style.display = 'block';
+        } else {
+            slide.style.display = 'none';
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 5000);
+
+function composeEmail() {
+    window.location.href = "mailto:2k21cse063@kiot.ac.in";
+
+}
+
+
